9bb915f1ae85fbaca570268d523ccf091b6c4aca
unknown <maazsp17@gmail.com>
2026-09-11 18:35:59 +0500

    add fixed group data

--- scripts/seedGroupTickets.ts
+++ scripts/seedGroupTickets.ts
@@ -16,7 +16,7 @@ const TICKET_DATA = [
     airline: "Airblue",
     airlineCode: "PA",
     airlineLogo:
-      "https://logo.clearbit.com/airblue.com",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Airblue_logo.svg/1280px-Airblue_logo.svg.png",
     sector: "MUX-JED-MUX",
     outboundDate: "2026-09-18",
     outboundTime: "00:45 - 04:10",
@@ -36,7 +36,7 @@ const TICKET_DATA = [
     airline: "Airblue",
     airlineCode: "PA",
     airlineLogo:
-      "https://logo.clearbit.com/airblue.com",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Airblue_logo.svg/1280px-Airblue_logo.svg.png",
     sector: "MUX-JED-MUX",
     outboundDate: "2026-09-20",
     outboundTime: "00:45 - 04:10",
